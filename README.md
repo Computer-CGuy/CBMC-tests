826c2c6339094e7b1dd0935726bcbf72106c1100
Yash Jha <r22133yash@dpsrkp.net>
2021-06-23 19:02:59 +0530

    Large Arrays

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+# Tests
 
+1) xor-array-cmbc.c SUCCESS
+2) xor-array-32-length.c SUCCESS
+3) xor-largers-array-cbmc.c SUCCESS
+4) xor-2D-array.c SUCCESS
+
+
+# SAT Solvers
 SCryptoMiniSat http://amit.metodi.me/research/scrypto/
--- xor-large-arrays-cbmc.c
+++ xor-large-arrays-cbmc.c
@@ -19,31 +19,37 @@ int main(){
 
 	int length_of_arr = 65;
 
-	int arr[] = {100,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1\
-				,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+	// int arr[] = {100,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1\
+	// 			,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
 	// int N = 0b11100;
 	// int N = nondet_int();
 
-	int new_arr[3] = {nondet_int()};
+	int arr[32];// = {1};
+	for(int i=0;i<32;i++){
+		arr[i]=1;
+	}
+	arr[0]=100;
 
-	__CPROVER_assume((arr*+0)>0&&(arr*+1)>0&&(arr*+2)>0);
+	int new_arr[2] = {nondet_int()};
+
+	// __CPROVER_assume(arr[0]>0&&arr[0]);
 	
 	int xorEd = 0;
 
 	
-	for(int y=0;y<3;y++){
-		for(int x=0;x<mon65;x++){
+	for(int y=0;y<2;y++){
+		for(int x=0;x<16;x++){
 			// printf("%d %d\n",(N&(1<<(x+100000))),arr[x]);
-			if((N&(1<<(x)))!=0){
+			if((new_arr[y]&(1<<(x)))!=0){
 			// if(N&x==0){
-				xorEd = (xorEd^arr[3*32+x]);
+				xorEd = (xorEd^arr[y*16+x]);
 			}
 		}
 	}
 
 	int flag = 0;
 
-	if((xorEd)!=100){
+	if((xorEd)!=1){
 		flag = 1;
 	}
 	assert(flag == 1);
