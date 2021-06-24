66f3da255bbeaa0d92a55af2139a9984131d466c
Yash Jha <r22133yash@dpsrkp.net>
2021-06-24 18:09:37 +0530

    Implemented Just nonce for Blake2B

--- README.md
+++ README.md
@@ -6,5 +6,13 @@
 4) xor-2D-array.c SUCCESS
 5) xor-array-bool.c SLOWER THAN xor-array-cmbc.c
 6) xor-custom-array.c x_1||x_2||x_3||x_4||x_5 == 0; x_i=?
+
+
+# ZCash Notes
+1) z=200,n=9
+2) How to digest? https://github.com/mjosaarinen/blake2_mjosref/blob/3491779dbf2465596b3db121701922cf7a9fd0a2/blake2b.c#L162  https://github.com/franziskuskiefer/Blake2b/blob/6d888db4f41523117e3a3cb110845bd0de392b74/src/blake2b_functions.c#L159
+3) May be useful. https://github.com/buggywhip/blake2_py/blob/master/blake2.py
+
 # SAT Solvers
 SCryptoMiniSat http://amit.metodi.me/research/scrypto/
+
--- blake2b-test.py
+++ blake2b-test.py
@@ -1,27 +1,10 @@
+"""Source:https://www.oryx-embedded.com/doc/blake2b_8c_source.html"""
+
 import numpy as np
 import struct
-from utils import G
+from utils import G,sigma,iv
 
-sigma = [
-   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-   [14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
-   [11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4],
-   [7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8],
-   [9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13],
-   [2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9],
-   [12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11],
-   [13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10],
-   [6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5],
-   [10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0],
-   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-   [14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3]
-]
 
-iv = \
-[
-    0x6A09E667F3BCC908, 0xBB67AE8584CAA73B, 0x3C6EF372FE94F82B, 0xA54FF53A5F1D36F1,
-    0x510E527FADE682D1, 0x9B05688C2B3E6C1F, 0x1F83D9ABFB41BD6B, 0x5BE0CD19137E2179
-]
 sigma = np.array(sigma,dtype=np.uint8)
 
 class context_obj(object):
@@ -36,7 +19,7 @@ def init(context,key,keyLen,digestLen):
 	l = iv.copy()
 	l[0]=l[0]^digestLen^0x01010000
 	context.h[:] = l
-	print(context.h)
+	# print(context.h)
 	# context.h[0]=(context.h[0]^digestLen^0x01010000)
 
 def update(context,data,length):
@@ -53,9 +36,9 @@ def update(context,data,length):
 
 		data = data[n:]
 		length-=n
-	if(context.size==128):
-		blake2bProcessBlock(context)
-		context.size = 0
+	
+	blake2bProcessBlock(context,True)
+	context.size = 0
 
 def blake2bProcessBlock(context,last=False):
 	v = np.zeros(16,dtype=np.uint64)
@@ -76,7 +59,7 @@ def blake2bProcessBlock(context,last=False):
 
 	# m = context.m
 	m = struct.unpack_from('<16%s' % 'Q', bytes(context.buffer))	
-
+	print(m)
 	for i in range(12):
 		G(v,0, 4, 8,  12, m[sigma[i][0]], m[sigma[i][1]]);
 		G(v,1, 5, 9,  13, m[sigma[i][2]], m[sigma[i][3]]);
@@ -92,11 +75,46 @@ def blake2bProcessBlock(context,last=False):
 	
 	for i in range(8):
 		context.h^=v[i]^v[i+8]
+"""
+# Missed
+ void blake2bFinal(Blake2bContext *context, uint8_t *digest)
+ {
+    size_t i;
+  
+    //The last block is padded with zeros to full block size, if required
+    for(i = context->size; i < 128; i++)
+    {
+       context->buffer[i] = 0;
+    }
+  
+    //Compress the last block
+    blake2bProcessBlock(context, TRUE);
+  
+    //Convert from host byte order to big-endian byte order
+    for(i = 0; i < 8; i++)
+    {
+       context->h[i] = htole64(context->h[i]);
+    }
+  
+    //Copy the resulting digest
+    if(digest != NULL)
+    {
+       osMemcpy(digest, context->digest, context->digestSize);
+    }
+ }
+"""
 
 cntx = context_obj()
-data = np.array([1]*128, dtype=np.uint8)
+# number<<8+nonce
+# data = np.array([1]*16+[3]+[1], dtype=np.uint32)
+print([(1<<8)+3])
+data = np.array([1]*16+[(1<<8)+4], dtype=np.uint32)
 length=len(data)
 init(cntx, 0, 0, 64)
 update(cntx, data, length)
 # blake2bProcessBlock
-print(cntx.h)
+# print(cntx.h)
+
+
+
+# 4/7
